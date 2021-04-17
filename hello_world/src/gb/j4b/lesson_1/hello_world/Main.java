ad06fb25e8fb74959bb1956473a1fcbe3ca3d467
Nickolay <nickolaypopov@gmail.com>
2021-04-17 15:31:38 +0500

    Lesson_1
    Hello world!

---
+++ hello_world/src/gb/j4b/lesson_1/hello_world/Main.java
@@ -0,0 +1,8 @@
+package gb.j4b.lesson_1.hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello world!!!");
+    }
+}
---
+++ homework_1/src/gb/j4b/lesson_1/homework/Main.java
@@ -0,0 +1,8 @@
+package gb.j4b.lesson_1.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Я буду изучать Java!");
+    }
+}
